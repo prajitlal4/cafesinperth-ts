18bffbe572de267035b1dc27305777f326701579
prajitlal4 <prajit.lal4@gmail.com>
2023-11-15 17:18:42 +0800

    fixed py in FullReview

--- src/app/components/FullReview.tsx
+++ src/app/components/FullReview.tsx
@@ -3,7 +3,7 @@ import { CheckCircleIcon, InformationCircleIcon } from '@heroicons/react/20/soli
 
 export default function FullReview( { review }: { review: any }) {
   return (
-    <div className="px-6 py-8 lg:px-8 lg:pt-36">
+    <div className="px-6 py-8 lg:px-8 lg:py-24">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <article>
           <h1 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 lg:text-5xl">{review.title}</h1>
