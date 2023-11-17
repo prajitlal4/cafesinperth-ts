6bdd0343fb2294e4bfa595021b25005672d5bfa9
prajitlal4 <prajit.lal4@gmail.com>
2023-11-17 21:53:48 +0800

    minor style changes

--- src/app/components/FullReview.tsx
+++ src/app/components/FullReview.tsx
@@ -27,19 +27,19 @@ export default async function FullReview( { review }: { review: any }) {
         </article>
         <div className="flex justify-between w-full flex-nowrap">
           {prev && (
-            <span className="flex-1 flex-grow py-8 rounded-xl mt-8 outline outline-gray-200 mr-4 lg:mr-10 ">
-              <Link href={`/review/${prev.slug}`} className="flex text-gray-300 hover:text-gray-700 ml-5">
+            <span className="flex-1 flex-grow py-8 rounded-xl mt-8 outline outline-gray-100 mr-4 lg:mr-10 transition-colors duration-150 ease-in-out hover:bg-gray-50">
+              <Link href={`/review/${prev.slug}`} className="flex text-gray-900  ml-5">
                 <span className="text-sm font-medium">Previous Review</span>
               </Link>
-              <Link href={`/review/${prev.slug}`} className="block mt-2 text-md font-semibold text-gray-900 hover:text-gray-700 ml-5 mr-5">{prev.title}</Link>
+              <Link href={`/review/${prev.slug}`} className="block mt-2 text-md font text-gray-900 hover:text-gray-700 ml-5 mr-5">{prev.title}</Link>
             </span>
           )}
           {next && (
-            <span className="flex-1 flex-grow py-8 text-right rounded-xl outline outline-gray-200 mt-8 ml-4 lg:ml-10">
-              <Link href={`/review/${next.slug}`} className="flex justify-end text-gray-300 hover:text-gray-700 mr-5">
+            <span className="flex-1 flex-grow py-8 text-right rounded-xl outline outline-gray-100 mt-8 ml-4 lg:ml-10 transition-colors duration-150 ease-in-out hover:bg-gray-50">
+              <Link href={`/review/${next.slug}`} className="flex justify-end text-gray-900 mr-5">
                 <span className="text-sm font-medium">Next Review</span>
               </Link>
-              <Link href={`/review/${next.slug}`} className="block mt-2 text-md font-semibold text-gray-900 hover:text-gray-700 mr-5 ml-5">{next.title}</Link>
+              <Link href={`/review/${next.slug}`} className="block mt-2 text-md font text-gray-900 hover:text-gray-700 mr-5 ml-5">{next.title}</Link>
             </span>
           )}
         </div>
