7c5e13681c1eff46f156b445ebf87a6769a782b5
prajitlal4 <prajit.lal4@gmail.com>
2023-11-19 10:31:49 +0800

    made entire div clickable

--- src/app/components/FeaturedReview.tsx
+++ src/app/components/FeaturedReview.tsx
@@ -8,51 +8,53 @@ export default function FeaturedReview({review} : { review: FeaturedReviewPost})
 
   return (
     <article className="flex flex-col items-start justify-between">
-      <div className="relative w-full">
-        <Image
-          src={review.imageUrl}
-          alt=""
-          height={300}
-          width={500}
-          className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-        />
-        <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-      </div>
-      <div className="max-w-xl">
-        <div className="mt-8 flex items-center gap-x-4 text-xs">
-          <p className="text-gray-500">
-            {publishedDate}
-          </p>
-          <p className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-            {review.location}
-          </p>
-        </div>
-        <div className="group relative">
-          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-            <Link href={`review/${review.slug}`}>
-              <span className="absolute inset-0" />
-              {review.title}
-            </Link>
-          </h3>
-          <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-            {review.description}
-          </p>
-        </div>
-        <div className="relative mt-8 flex items-center gap-x-4">
+      <Link href={`review/${review.slug}`}>
+        <div className="relative w-full">
           <Image
-            src={review.author.image}
+            src={review.imageUrl}
             alt=""
-            height={40}
-            width={40}
-            className="h-10 w-10 rounded-full bg-gray-100"
+            height={300}
+            width={500}
+            className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
           />
-          <div className="text-sm leading-6">
-            <p className=" text-gray-900">
-              {review.author.name}
+          <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+        </div>
+        <div className="max-w-xl">
+          <div className="mt-8 flex items-center gap-x-4 text-xs">
+            <p className="text-gray-500">
+              {publishedDate}
+            </p>
+            <p className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+              {review.location}
             </p>
           </div>
+          <div className="group relative">
+            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+              <Link href={`review/${review.slug}`}>
+                <span className="absolute inset-0" />
+                {review.title}
+              </Link>
+            </h3>
+            <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+              {review.description}
+            </p>
+          </div>
+          <div className="relative mt-8 flex items-center gap-x-4">
+            <Image
+              src={review.author.image}
+              alt=""
+              height={40}
+              width={40}
+              className="h-10 w-10 rounded-full bg-gray-100"
+            />
+            <div className="text-sm leading-6">
+              <p className=" text-gray-900">
+                {review.author.name}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
+      </Link>
     </article>
   );
 }
