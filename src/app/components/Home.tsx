cfcb068777f832d51861ecf7e531a11e86ff0446
prajitlal4 <prajit.lal4@gmail.com>
2023-11-20 20:21:50 +0800

    changed navbar and alignment

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -6,27 +6,19 @@ export default function HomeComponent() {
   return (
     <div className="">
       <div className="relative isolate overflow-hidden pt-14">
-        <Image
-              src='https://cafesinperth.syd1.cdn.digitaloceanspaces.com/cafesinperthbg2.png'
-              width={1920}
-              height={1080}
-              alt=""
-              className="absolute inset-0 -z-10 h-full w-full object-cover"
-              priority
-        />
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-32">
           <div className="text-center  py-5 mx-3 rounded-2xl bg-opacity-80">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl px-3 py-2 drop-shadow-2xl bg-black bg-opacity-75 rounded-2xl">
+            <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl px-3 py-2 drop-shadow-2xl">
               Your one source of truth for the best Cafes in Perth.
             </h1>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 href={`/review`}
-                className="flex-none rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white bg-opacity-75 shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black bg-opacity-75 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
               >
                 View Cafes <span aria-hidden="true">→</span>
               </Link>
-              <Link href={`#subscribe`} className="flex-none rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white bg-opacity-75 shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+              <Link href={`#subscribe`} className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black bg-opacity-75 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
                 Stay up to date! <span aria-hidden="true">→</span>
               </Link>
             </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,54 +17,19 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="sticky w-full bg-white shadow-sm top-0 z-50">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header className="w-full bg-white top-0 z-50 flex">
+      <nav className="mx-auto flex max-w-7xl p-6 lg:px-8" aria-label="Global">
         <Link href={`/`} className="-m-1.5 p-1.5">
           <span className="sr-only">Cafes in Perth</span>
           <Image 
-            className="h-10 md:h-16 w-auto" 
+            className="h-12 md:h-20 w-auto" 
             src="https://cafesinperth.syd1.cdn.digitaloceanspaces.com/Capture.PNG" 
             alt="Cafes in Perth Logo"
             height={800}
             width={800} 
           />
         </Link>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link href={`/`} className="-m-1.5 p-1.5">
-              <span className="sr-only">Cafes in Perth</span>
-              <Image
-                className="h-8 w-auto"
-                src="https://cafesinperth.syd1.cdn.digitaloceanspaces.com/Capture.PNG"
-                alt=""
-                height={800}
-                width={800}
-              />
-            </Link>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
   )
 }
