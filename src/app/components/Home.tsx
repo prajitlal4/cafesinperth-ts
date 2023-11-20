8315b89b3c6924461359c290fd9c45db25434108
prajitlal4 <prajit.lal4@gmail.com>
2023-11-20 19:10:48 +0800

    changed BG image.

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -4,11 +4,18 @@ import Link from "next/link";
 export default function HomeComponent() {
 
   return (
-    <div className="bg-gray-900">
+    <div className="">
       <div className="relative isolate overflow-hidden pt-14">
+        <Image
+              src='https://cafesinperth.syd1.cdn.digitaloceanspaces.com/cafesinperthbg2.png'
+              width={1920}
+              height={1080}
+              alt=""
+              className="absolute inset-0 -z-10 h-full w-full object-cover"
+        />
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center  py-5 mx-3 rounded-2xl bg-opacity-80">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl px-3">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl px-3 py-2 drop-shadow-2xl bg-black bg-opacity-60 rounded-2xl">
               Your one source of truth for the best Cafes in Perth.
             </h1>
             <div className="mt-10 flex items-center justify-center gap-x-6">
